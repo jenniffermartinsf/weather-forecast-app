51f4012169de4899f5fae6f20954a0ebd3e6c976
Jenniffer Martins <eujennifferlino@gmail.com>
2026-03-14 20:28:55 -0300

    feat: setup viewcode
    - remove storyboard

---
+++ weather-forecast-app/weather-forecast-app/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  weather-forecast-app
+//
+//  Created by Jenniffer Martins on 13/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private lazy var customView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    
+    private func setupView() {
+        view.backgroundColor = .orange
+        
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        view.addSubview(customView)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+        ])
+    }
+    
+    
+
+
+}
+
